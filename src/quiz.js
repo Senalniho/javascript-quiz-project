34a7070eafdef35a0d1f8f39a5111f2f7369e3db
Senanu Adjabeng <adjabengsenanu@yahoo.com>
2024-06-24 17:46:33 +0200

    My Day One Solution

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,55 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
 
-    // 5. checkAnswer(answer)
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
+  }
 
-    // 6. hasEnded()
+  checkAnswer(answer) {
+    const currentQuestion = this.getQuestion();
+    if (currentQuestion.answer === answer) {
+      this.correctAnswers++;
+    }
+  }
+
+  hasEnded() {
+    return (
+      this.currentQuestionIndex >= this.questions.length ||
+      this.timeRemaining <= 0
+    );
+  }
 }
+
+// YOUR CODE HERE:
+//
+// 1. constructor (questions, timeLimit, timeRemaining)
+
+// 2. getQuestion()
+
+// 3. moveToNextQuestion()
+
+// 4. shuffleQuestions()
+
+// 5. checkAnswer(answer)
+
+// 6. hasEnded()
